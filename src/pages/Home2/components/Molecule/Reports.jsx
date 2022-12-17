df318e9f9ba443621490428dacccda30bd88526e
nguyenduong1803 <duonghd1803@gmail.com>
2022-12-17 23:39:25 +0700

    fix: config folder

---
+++ src/pages/Home2/components/Molecule/Reports.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Android, BugIcon, MacIcon, WindowIcon } from '~/assets/svg';
+import { Col, Grid } from '~/layouts/Comp/GridSystem';
+import Paper from '~/layouts/Styled/Paper';
+import InsetColor from '~/layouts/Styled/Shapes/InsetColor';
+const list = [
+  {
+    icon: Android,
+    name: 'Weekly Sales',
+    total: '786k',
+    color: 'rgb(16, 57, 150)',
+    bg: 'rgba(16, 57, 150, 0.24)',
+    wrapColor: 'rgb(209, 233, 252)',
+  },
+  {
+    icon: MacIcon,
+    name: 'New users',
+    total: '1.35M',
+    color: 'rgb(12, 83, 183) ',
+    bg: 'rgba(16, 57, 150, 0.24)',
+    wrapColor: 'rgb(208, 242, 255)',
+  },
+  {
+    icon: WindowIcon,
+    name: 'Item orders',
+    total: '1.22M',
+    color: 'rgb(183, 129, 3)',
+    bg: 'rgba(183, 129, 3, 0.24)',
+    wrapColor: 'rgb(255, 247, 205);',
+  },
+  {
+    icon: BugIcon,
+    name: 'Bug report',
+    total: '186k',
+    color: 'rgb(183, 33, 54)',
+    bg: 'rgba(183, 33, 54, 0.24)',
+    wrapColor: 'rgb(255, 231, 217)',
+  },
+];
+function Reports() {
+  return (
+    <Grid className="mb-24">
+      {list.map((item, index) => {
+        const Icon = item.icon
+        return (
+          <Col key={index}>
+            <Paper elevation={0} r={10} bg={item.wrapColor}>
+              <div className="d-flex p-24 flex-direction-column a-center">
+                <InsetColor color={item.color} bg={item.bg}>
+                  <Icon />
+                </InsetColor>
+                <h2>{item.total}</h2>
+                <p>{item.name}</p>
+              </div>
+            </Paper>
+          </Col>
+        );
+      })}
+    </Grid>
+  )
+}
+
+export default Reports
