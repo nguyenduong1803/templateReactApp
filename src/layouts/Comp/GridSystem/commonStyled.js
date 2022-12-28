f4c5c5c881298cbfc43d6148927de6c0c1572630
duonnh200 <duongnh@sphinxjsc.com>
2022-12-28 09:41:07 +0700

    fix commonStyled styled

--- src/layouts/Comp/GridSystem/commonStyled.js
+++ src/layouts/Comp/GridSystem/commonStyled.js
@@ -16,6 +16,9 @@ const setStyleComponent = ({ radius, ...props }) => {
     ...props.sx,
     '@media (min-width:62em)and (max-width: 74.9375em)': props.lg,
     '@media (min-width:48em)and (max-width: 61.9375em)': props.md,
+    '@media (min-width: 36em) and (max-width: 47.9375em)': props.sm,
+    '@media  (max-width: 35.9375em)': props.xs,
+    
   };
 };
 const CommonStyled = styled.div(setStyleComponent);
