674be5c7d8a4dba6ebf634811b21e858633fadd0
nguyenduong1803 <duonghd1803@gmail.com>
2023-01-11 09:19:48 +0700

    fix: Tabs Table

--- src/layouts/LayoutSidebar/Addendum.jsx
+++ src/layouts/LayoutSidebar/Addendum.jsx
@@ -1,38 +1,37 @@
 import React from 'react';
 import { Box } from '../Grid';
-import Paper from '../Styled/Paper';
 
 function Addendum() {
   return (
-    <Box m='65px 0 0 0' className='vh-100 position-fixed t-0 p-12 zIndex-100 '>
+    <Box m='65px 0 0 0' className='vh-100 position-fixed t-0 zIndex-100 '>
       <ul>
         <li>
-          <a className='line-height-32 d-block' href=''>
+          <a className='line-height-32 d-block' href='/'>
             Basic Accodion
           </a>
         </li>
         <li>
-          <a className='line-height-32 d-block' href=''>
+          <a className='line-height-32 d-block' href='/'>
             Controlled accordion
           </a>
         </li>
         <li>
-          <a className='line-height-32 d-block' href=''>
+          <a className='line-height-32 d-block' href='/'>
             Customization
           </a>
         </li>
         <li>
-          <a className='line-height-32 d-block' href=''>
+          <a className='line-height-32 d-block' href='/'>
             Performance
           </a>
         </li>
         <li>
-          <a className='line-height-32 d-block' href=''>
+          <a className='line-height-32 d-block' href='/'>
             Accessibility
           </a>
         </li>
         <li>
-          <a className='line-height-32 d-block' href=''>
+          <a className='line-height-32 d-block' href='/'>
             API
           </a>
         </li>
--- src/pages/Components/Organism/Table.jsx
+++ src/pages/Components/Organism/Table.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import Paper from '~/layouts/Styled/Paper';
-function Table({ labels }) {
+import PropTypes from 'prop-types';
+function Table({ labels, borderColor, p }) {
   return (
     <Paper r={6}>
-      <Wrap>    
+      <Wrap>
         <thead>
           <Tr>
             {labels.map((item) => {
               return (
-                <Th className='fw-600' key={item}>
+                <Th p={p} className='fw-600' key={item}>
                   {item}
                 </Th>
               );
@@ -18,25 +19,25 @@ function Table({ labels }) {
         </thead>
         <tbody>
           <tr>
-            <Td>Ripple</Td>
-            <Td>Boolean</Td>
-            <Td>Display of the ripple when click</Td>
-            <Td>False</Td>
-            <Td>1</Td>
+            <Td p={p}>Ripple</Td>
+            <Td p={p}>Boolean</Td>
+            <Td p={p}>Display of the ripple when click</Td>
+            <Td p={p}>False</Td>
+            <Td p={p}>1</Td>
           </tr>
           <tr>
-            <Td>Ripple</Td>
-            <Td>Boolean</Td>
-            <Td>Display of the ripple when click</Td>
-            <Td>False</Td>
-            <Td>1</Td>
+            <Td p={p}>Ripple</Td>
+            <Td p={p}>Boolean</Td>
+            <Td p={p}>Display of the ripple when click</Td>
+            <Td p={p}>False</Td>
+            <Td p={p}>1</Td>
           </tr>
           <tr>
-            <Td>Ripple</Td>
-            <Td>Boolean</Td>
-            <Td>Display of the ripple when click</Td>
-            <Td>False</Td>
-            <Td>1</Td>
+            <Td p={p}>Ripple</Td>
+            <Td p={p}>Boolean</Td>
+            <Td p={p}>Display of the ripple when click</Td>
+            <Td p={p}>False</Td>
+            <Td p={p}>1</Td>
           </tr>
         </tbody>
       </Wrap>
@@ -44,9 +45,16 @@ function Table({ labels }) {
   );
 }
 
+Table.propTypes = {
+  labels: PropTypes.array
+};
+Table.defaultProps = {
+  labels: [],
+  p: '6px'
+};
+
 const Wrap = styled.table`
   width: 100%;
-  border: 1px solid #ddd;
   border-collapse: collapse;
   border-spacing: 0;
 `;
@@ -55,12 +63,12 @@ const Tr = styled.tr`
 `;
 const Td = styled.td`
   border: 1px solid #ddd;
-  padding: 6px;
+  padding: ${({ p }) => p};
   text-align: center;
 `;
 const Th = styled.td`
   border: 1px solid #ddd;
-  padding: 6px;
+  padding: ${({ p }) => p};
   text-align: center;
 `;
 export default Table;
